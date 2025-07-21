a9acaea7fb5ad59870b1a9033695483897a39e1c
nileshh0027 <nbagda17@gmail.com>
2025-07-21 09:07:37 +0530

    Add a new file

---
+++ JS/Variables.js
@@ -0,0 +1,15 @@
+var number = 123
+const cityCode = 2761746
+let city = "Ahmedabad"
+let number1 = 1234
+// cityCode = 380009  can't assign to const variable
+
+console.log(number)
+console.log(cityCode);
+console.log(city);
+console.log(number1);
+
+
+
+
+
